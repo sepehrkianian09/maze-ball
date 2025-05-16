3605bcba3f0e6532490a54daf4edd1c63ce7a24a
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-16 13:40:29 +0000

    feat(game): init forge2d game instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(GameWidget.controlled(gameFactory: Forge2DGame.new));
 }
