6e3f4e7b60be4cc939c281e00fda1d5449ec3a1f
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-16 15:39:03 +0000

    feat(game): init Maze

--- lib/components/game.dart
+++ lib/components/game.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:maze_ball/components/maze.dart';
 
 import 'background.dart';
 import 'ball.dart';
@@ -20,14 +21,7 @@ class MazeBallGame extends Forge2DGame {
     final backgroundImage = await images.load('background/colored_grass.png');
     await world.add(Background(sprite: Sprite(backgroundImage)));
 
-    final gameRect = camera.visibleWorldRect;
-    await world.add(
-      MazeTile(
-        position: Vector2(0, gameRect.bottom - 4),
-        size: Vector2(10, 2),
-        color: Colors.amber,
-      ),
-    );
+    await world.add(Maze());
 
     await world.add(
       Ball(position: Vector2.zero(), size: 10, color: Colors.brown),
---
+++ lib/components/maze.dart
@@ -0,0 +1,31 @@
+import 'package:flame/components.dart';
+import 'package:flame_forge2d/body_component.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/material.dart';
+import 'package:maze_ball/components/game.dart';
+import 'package:maze_ball/components/maze_tile.dart';
+
+class Maze extends BodyComponent<MazeBallGame> {
+  Maze() : super(bodyDef: BodyDef());
+
+  @override
+  Future<void> onLoad() async {
+    final gameRect = game.camera.visibleWorldRect;
+    final gameWidth = gameRect.right - gameRect.left;
+    for (var i = 0; i <= 4; i++) {
+      final tileSize = Vector2(10, 2);
+      await add(
+        MazeTile(
+          position: Vector2(
+            gameRect.left + (i * gameWidth / 4),
+            gameRect.bottom - 4,
+          ),
+          size: tileSize,
+          color: Colors.amber,
+        ),
+      );
+    }
+
+    return super.onLoad();
+  }
+}
