6f53d1f4ba7f90f7585331488de63c0b8dda2071
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-16 15:49:47 +0000

    feat(maze_tile): init random angle construction

--- lib/components/maze.dart
+++ lib/components/maze.dart
@@ -7,6 +7,14 @@ import 'package:flutter/material.dart';
 import 'package:maze_ball/components/game.dart';
 import 'package:maze_ball/components/maze_tile.dart';
 
+enum MazeTileAngle {
+  zero(0),
+  perpendicular(pi / 2);
+
+  final double value;
+  const MazeTileAngle(this.value);
+}
+
 class Maze extends BodyComponent<MazeBallGame> {
   Maze() : super(bodyDef: BodyDef());
 
@@ -16,15 +24,18 @@ class Maze extends BodyComponent<MazeBallGame> {
     final gameWidth = gameRect.right - gameRect.left;
     for (var i = 0; i <= 4; i++) {
       final tileSize = Vector2(10, 2);
+      randomAngle() =>
+          MazeTileAngle.values[Random().nextInt(MazeTileAngle.values.length)];
+      final angle = randomAngle();
       await add(
         MazeTile(
           position: Vector2(
-            gameRect.left + (i * gameWidth / 4),
+            gameRect.left + (i * gameWidth / 4) + (angle == MazeTileAngle.perpendicular ? 0 : tileSize.x / 2),
             gameRect.bottom - 4,
           ),
           size: tileSize,
           color: Colors.amber,
-          angle: pi / 2
+          angle: angle.value,
         ),
       );
     }
