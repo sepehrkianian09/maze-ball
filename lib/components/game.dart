626a5aeb1610203ed47acbaa3ac7bc4ec9cc6636
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-16 13:49:31 +0000

    feat(game): add background

--- lib/components/game.dart
+++ lib/components/game.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+
+import 'background.dart';
 
 class MazeBallGame extends Forge2DGame {
   MazeBallGame()
@@ -7,4 +11,12 @@ class MazeBallGame extends Forge2DGame {
         gravity: Vector2(0, 10),
         camera: CameraComponent.withFixedResolution(width: 800, height: 600),
       );
+
+  @override
+  FutureOr<void> onLoad() async {
+    final backgroundImage = await images.load('colored_shroom.png');
+    await world.add(Background(sprite: Sprite(backgroundImage)));
+
+    return super.onLoad();
+  }
 }
