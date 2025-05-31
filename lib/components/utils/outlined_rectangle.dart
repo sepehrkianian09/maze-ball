ceee1a13f73137fe2f132397e6a6404f0f00e11d
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-31 21:55:10 +0000

    feat(outlined-rectangle): add anchor

--- lib/components/utils/outlined_rectangle.dart
+++ lib/components/utils/outlined_rectangle.dart
@@ -8,6 +8,7 @@ class OutlinedRectangleComponent extends PositionComponent {
   OutlinedRectangleComponent({
     required Vector2 position,
     required Vector2 size,
+    super.anchor = Anchor.topLeft,
     this.color = Colors.white,
     this.strokeWidth = 2,
   }) {
@@ -19,11 +20,35 @@ class OutlinedRectangleComponent extends PositionComponent {
   void render(Canvas canvas) {
     super.render(canvas);
 
-    final paint = Paint()
-      ..color = color
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth;
+    final paint =
+        Paint()
+          ..color = color
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = strokeWidth;
 
-    canvas.drawRect(size.toRect(), paint);
+    // canvas.drawRect(size.toRect(), paint);
+    canvas.drawPath(_getPath(sizeToVertices(size, anchor)), paint);
+  }
+
+  Path _getPath(List<Vector2> vertices) {
+    final path = Path()..moveTo(vertices[0].x, vertices[0].y);
+
+    for (int i = 1; i < vertices.length; i++) {
+      path.lineTo(vertices[i].x, vertices[i].y);
+    }
+    path.close(); // optional, closes the shape
+
+    return path;
+  }
+
+  @protected
+  static List<Vector2> sizeToVertices(Vector2 size, Anchor? componentAnchor) {
+    final anchor = componentAnchor ?? Anchor.topLeft;
+    return [
+      Vector2(-size.x * anchor.x, -size.y * anchor.y),
+      Vector2(-size.x * anchor.x, size.y - size.y * anchor.y),
+      Vector2(size.x - size.x * anchor.x, size.y - size.y * anchor.y),
+      Vector2(size.x - size.x * anchor.x, -size.y * anchor.y),
+    ];
   }
 }
